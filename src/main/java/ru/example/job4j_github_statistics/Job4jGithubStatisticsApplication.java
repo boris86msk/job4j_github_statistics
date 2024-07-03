1afd47a77ec5e7c3c8e793da124979b83a24c0cb
boris86msk <boris86msk@yandex.ru>
2024-07-03 22:07:40 +0300

    init commit

---
+++ src/main/java/ru/example/job4j_github_statistics/Job4jGithubStatisticsApplication.java
@@ -0,0 +1,13 @@
+package ru.example.job4j_github_statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jGithubStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jGithubStatisticsApplication.class, args);
+    }
+
+}
