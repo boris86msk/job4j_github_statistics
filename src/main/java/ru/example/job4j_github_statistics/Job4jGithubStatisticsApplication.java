29aeb54432a8eb8da3da0dc193d822243d179a06
boris86msk <boris86msk@yandex.ru>
2024-07-13 23:14:21 +0300

    add Scheduled

--- src/main/java/ru/example/job4j_github_statistics/Job4jGithubStatisticsApplication.java
+++ src/main/java/ru/example/job4j_github_statistics/Job4jGithubStatisticsApplication.java
@@ -2,8 +2,10 @@ package ru.example.job4j_github_statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Job4jGithubStatisticsApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/example/job4j_github_statistics/service/ScheduledTasks.java
+++ src/main/java/ru/example/job4j_github_statistics/service/ScheduledTasks.java
@@ -1,15 +1,22 @@
 package ru.example.job4j_github_statistics.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import ru.example.job4j_github_statistics.model.User;
+import ru.example.job4j_github_statistics.repository.UserRepository;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class ScheduledTasks {
-    private final RepositService repositoryService;
+    private final RepositService repositService;
     private final GitHubRemote gitHubRemote;
+    private final UserRepository userRepository;
 
+    @Scheduled(fixedDelayString = "P1D")
     public void fetchCommits() {
-
+        List<User> userList = userRepository.findAll();
     }
 }
